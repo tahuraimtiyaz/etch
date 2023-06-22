55de7b099dba4ee01c3b25224ddf5b386f8339e9
tahuraimtiyaz <tahuraimtiyaz@gmail.com>
2023-06-22 17:35:56 +0530

    fixed

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let color = "black";
-let click = true;
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function(){
-    createBoard(64);
+    createBoard(16);
 
-    document.querySelector("body").addEventListener("click", function(e) {
+    document.querySelector("body").addEventListener("click", function(e){
         if(e.target.tagName != "BUTTON"){
             click = !click;
             let draw = document.querySelector("#draw");
-            if (click){
+            if(click){
                 draw.innerHTML = "Now You Can Draw";
             }
             else{
-                draw.innerHTML = "you're Now Allowed To Draw";
+                draw.innerHTML = "You're Now Allowed To Draw"
             }
         }
     })
@@ -27,51 +27,50 @@ document.addEventListener("DOMContentLoaded", function(){
 function createBoard(size){
     let board = document.querySelector(".board");
 
-    board.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows =`repeat(${size}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`; 
 
     let numDivs = size * size;
-     for (let i =0; i < numDivs; i++){
+
+    for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
         div.addEventListener("mouseover", colorDiv)
         board.insertAdjacentElement("beforeend", div);
-     }
-    
+    }
+
 }
 
-function getSize () {
+function getSize(){
     let input = prompt("What will be the size of the board?");
     let message = document.querySelector("#message");
-    if(input == "") {
-       message.innerHTML = "Please provide a number";
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
     }
-    else if(input < 0 || input > 100) {
+    else if(input < 0 || input > 100){
         message.innerHTML = "Provide a number between 1 and 100"
     }
-    else {
+    else{
         message.innerHTML = "Now you play!"
+        return input;
     }
-
 }
 
 function colorDiv(){
     if(click){
-    if(color == "random"){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else {
-        this.style.backgroundColor = 'black'
-    }
+        if(color == "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else{
+            this.style.backgroundColor = 'black'
+        }
     }
 }
 
 function setColor(colorChoice){
-    let color = colorChoice;
+    color = colorChoice;
 }
 
 function resetBoard(){
     let divs = document.querySelectorAll("div")
     divs.forEach((div) => div.style.backgroundColor = "white")
 }
-
-    
